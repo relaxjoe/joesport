6abe7cd3a5d459116518a7ac284db940039a6454
Joe <joeyndiebel@gmail.com>
2024-08-29 14:48:53 -0500

    center resume

--- src/components/banner/index.jsx
+++ src/components/banner/index.jsx
@@ -1,27 +1,57 @@
 function Banner() {
-
-    return <>
-    <section class="banner" data-scroll-index='0'>
-  <div class="banner-overlay">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-8 col-sm-12">
-          <div class="banner-text">
-            <h2 class="white">Joseph Diebel</h2> 
-            <h6 class="white">See more on <a href="https://github.com/relaxjoe?tab=repositories" target="_blank" rel="dofollow" class="weblink">github</a>.</h6>
-            <p class="banner-text white">Welcome to my portfolio, Below is my work and ways to contact me!</p>
-            <ul>
-              <li><a href="https://www.linkedin.com/in/joseph-diebel/" target="_blank" rel="dofollow" class="weblink"><img src="/images/resume.png" class="wow fadeInUp resume" data-wow-delay="0.4s" style={{ width: '500px', height: 'auto' }} /></a></li>
-              {/* <li><a href="#"><img src="images/.png" class="wow fadeInUp" data-wow-delay="0.7s"/></a></li>  */}
-            </ul>
+  return (
+    <>
+      <section class="banner" data-scroll-index="0">
+        <div class="banner-overlay">
+          <div class="container">
+            <div class="row">
+              <div class="mx-auto col-md-8 col-sm-12">
+                <div class="banner-text">
+                  <h2 class="white">Joseph Diebel</h2>
+                  <h6 class="white">
+                    See more on{" "}
+                    <a
+                      href="https://github.com/relaxjoe?tab=repositories"
+                      target="_blank"
+                      rel="dofollow"
+                      class="weblink"
+                    >
+                      github
+                    </a>
+                    .
+                  </h6>
+                  <p class="banner-text white">
+                    Welcome to my portfolio, Below is my work and ways to
+                    contact me!
+                  </p>
+                  <ul>
+                    <li>
+                      <a
+                        href="https://www.linkedin.com/in/joseph-diebel/"
+                        target="_blank"
+                        rel="dofollow"
+                        class="weblink"
+                      >
+                        <img
+                          src="/images/resume.png"
+                          class="wow fadeInUp resume"
+                          data-wow-delay="0.4s"
+                          style={{ width: "500px", height: "auto" }}
+                        />
+                      </a>
+                    </li>
+                    {/* <li><a href="#"><img src="images/.png" class="wow fadeInUp" data-wow-delay="0.7s"/></a></li>  */}
+                  </ul>
+                </div>
+              </div>
+              {/* <div class="col-md-4 col-sm-12"> <img src="/images/me.png" class="img-fluid wow fadeInUp responsive-images"/> </div> */}
+            </div>
           </div>
         </div>
-        <div class="col-md-4 col-sm-12"> <img src="/images/me.png" class="img-fluid wow fadeInUp responsive-images"/> </div>
-      </div>
-    </div>
-  </div>
-  {/* <span class="svg-wave"> <img class="svg-hero" src="images/applight-wave.svg"/> </span>*/} </section>
+        {/* <span class="svg-wave"> <img class="svg-hero" src="images/applight-wave.svg"/> </span>*/}{" "}
+      </section>
     </>
+  );
 }
 
 export default Banner;
